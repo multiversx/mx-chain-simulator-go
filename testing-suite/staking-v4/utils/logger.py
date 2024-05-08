11760efd6c85229a445ca1ffcad4e764036d0424
Mihail Lungu <lungumihai25@gmail.com>
2024-05-08 19:16:43 +0300

    Add logger method and log all actions in _48 test

--- testing-suite/staking-v4/utils/logger.py
+++ testing-suite/staking-v4/utils/logger.py
@@ -5,7 +5,7 @@ def get_logger(name):
     if not logger.handlers:  # Avoid adding multiple handlers to the same logger
         logger.setLevel(logging.DEBUG)
         ch = logging.StreamHandler()
-        ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        ch.setFormatter(logging.Formatter('[%(asctime)s] - [%(levelname)s] - %(message)s'))
         logger.addHandler(ch)
     return logger
 
