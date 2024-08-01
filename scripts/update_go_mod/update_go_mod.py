b237a8b1a7821bfd5f0d3ba7ff2db61561684ead
python-qa <mihail.lungu@multiversx.com>
2024-08-01 15:00:05 +0300

    update structure in order to pass the hash to a python script

--- scripts/update_go_mod/update_go_mod.py
+++ scripts/update_go_mod/update_go_mod.py
@@ -3,40 +3,6 @@ import re
 import subprocess
 import sys
 
-def get_current_branch(repo_path):
-    """
-    Get the current branch name of the Git repository.
-    """
-    try:
-        os.chdir(repo_path)
-        result = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if result.returncode != 0:
-            print(f"Failed to get the current branch name: {result.stderr.decode('utf-8')}")
-            sys.exit(1)
-        return result.stdout.decode('utf-8').strip()
-    except Exception as e:
-        print(f"Failed to determine the current branch: {str(e)}")
-        sys.exit(1)
-
-def get_latest_commit_hash(repo_path, branch_name):
-    """
-    Get the latest commit hash from the specified branch.
-    """
-    try:
-        os.chdir(repo_path)
-        # Fetch the latest changes to ensure you have the latest commits
-        subprocess.run(['git', 'fetch'], check=True)
-        # Checkout the branch explicitly to get the latest commit from that branch
-        subprocess.run(['git', 'checkout', branch_name], check=True)
-        result = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if result.returncode != 0:
-            print(f"Failed to get the latest commit hash: {result.stderr.decode('utf-8')}")
-            sys.exit(1)
-        return result.stdout.decode('utf-8').strip()
-    except Exception as e:
-        print(f"Failed to obtain the latest commit hash: {str(e)}")
-        sys.exit(1)
-
 def update_go_mod_file(go_mod_path, new_hash):
     """
     Update the go.mod file with the new commit hash.
@@ -59,22 +25,21 @@ def update_go_mod_file(go_mod_path, new_hash):
 if __name__ == "__main__":
     try:
         # Assuming the script is run from the root of mx-chain-simulator-go
-        repo_path = '../mx-chain-go'
         go_mod_path = './go.mod'
 
-        print(f"Repository path: {repo_path}")
         print(f"go.mod path: {go_mod_path}")
 
-        # Get the current branch name
-        current_branch = get_current_branch(repo_path)
-        print(f"Current branch: {current_branch}")
+        # Get the commit hash from the command-line argument
+        if len(sys.argv) != 2:
+            print("Usage: update_go_mod.py <commit_hash>")
+            sys.exit(1)
 
-        # Get the latest commit hash from the current branch
-        latest_commit_hash = get_latest_commit_hash(repo_path, current_branch)
+        latest_commit_hash = sys.argv[1]
+        print(f"Latest commit hash received: {latest_commit_hash}")
+
+        # Update the go.mod file with the provided commit hash
         update_go_mod_file(go_mod_path, latest_commit_hash)
 
-        # Output the commit hash for GitHub Actions
-        print(f"::set-output name=commit_hash::{latest_commit_hash}")
         print("Python Script executed successfully.")
     except Exception as e:
         print(f"Script execution failed: {str(e)}")
