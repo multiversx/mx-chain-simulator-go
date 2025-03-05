230feb1b0a1cf52c33f8ff30db584f9339d016ac
axenteoctavian <axente.octavian@gmail.com>
2025-03-05 09:29:44 +0200

    fix after review

--- scripts/prerequisites-sovereign.sh
+++ scripts/prerequisites-sovereign.sh
@@ -1,6 +1,7 @@
-cd ..
+SOVEREIGN_BRANCH="feat/chain-go-sdk"
 
+cd ..
 git clone https://github.com/multiversx/mx-chain-go.git
 cd mx-chain-go
 git fetch
-git checkout feat/chain-go-sdk
+git checkout $SOVEREIGN_BRANCH
